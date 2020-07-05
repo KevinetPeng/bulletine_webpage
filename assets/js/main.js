5cce813198380eec311bb8bdf3bd0e2b449ffa61
Jay <mantine08@gmail.com>
2020-07-04 23:59:42 -0700

    Added fade in transition

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,22 @@
       $("#topButton").animate({ bottom: "-70px" }, 3);
       $("#topButton").animate({ display: "none" }, 0);
     }
+
+    /* Check the location of each desired element */
+    $(".wrapper").each(function (i) {
+      var bottom_of_object = $(this).position().top + $(this).outerHeight() / 4;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate(
+          {
+            opacity: "1",
+          },
+          500
+        );
+      }
+    });
   });
 
   // Header.
